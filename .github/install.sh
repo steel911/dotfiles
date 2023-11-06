5f132720fecf2eb1921bd42d4f83b71ad5440daf
jijiang_zhao <jijiang_zhao@human-horizons.com>
2023-11-06 20:17:03 +0800

    upgrade to yadm; ongoning...

--- .github/install.sh
+++ .github/install.sh
@@ -7,7 +7,10 @@
 mkdir -p ~/.local/bin
 
 # Download yadm - yet another dotfile manager
-curl -sfLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
+curl \
+    --proxy "socks5://127.0.0.1:7890"   \
+    -sfLo ~/.local/bin/yadm             \
+    https://github.com/TheLocehiliosan/yadm/raw/master/yadm
 
 # +Execute permission
 chmod u+x ~/.local/bin/yadm
